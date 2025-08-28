16b4978afe853a389720dbfea7e846557ae43b5f
Gdev <125574259+gisachris@users.noreply.github.com>
2025-08-28 06:58:45 +0200

    setup an http get request

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -2,8 +2,13 @@ import express from 'express'
 import dotenv from 'dotenv'
 
 const App = express()
+const BaseURL = 'v1'
 
 dotenv.config()
+
+App.get(`/${BaseURL}/`, (req, res) => {
+    res.send({ color: `#${Math.floor(Math.random() * 16777215).toString(16)}` })
+})
 
 App.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
